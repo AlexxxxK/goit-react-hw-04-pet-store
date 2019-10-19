7768bc0dafce8bbc8244a2445d341ceafeec4b2c
Oleg Kozyr <olegvkozyr@gmail.com>
2019-10-19 19:12:34 +0300

    Updates

---
+++ precache-manifest.3b08af2473196f87f6ae1c210895e193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f857e4943c3c7e409c3a9c0915d012db",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "83647467eb4e3d825d12",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.954376a9.chunk.css"
+  },
+  {
+    "revision": "c2e0b636d5efdd5517dc",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.2a90a0c4.chunk.js"
+  },
+  {
+    "revision": "83647467eb4e3d825d12",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.fd845e6e.chunk.js"
+  },
+  {
+    "revision": "85d28d4578ed648bcf2a",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.03d6a3d1.js"
+  }
+]);
